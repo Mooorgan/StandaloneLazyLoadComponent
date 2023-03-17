81f84da67eb537f871254958afe20ef5202cdd48
ShaishavPaudel <morganhouse15@gmail.com>
2023-03-17 12:15:24 +0545

    DashBoardModule Declaration added

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { TodayComponent } from './today/today.component';
 
 @NgModule({
-  declarations: [DashboardComponent],
-  imports: [DashboardRoutingModule]
+  declarations: [DashboardComponent, TodayComponent],
+  imports: [DashboardRoutingModule],
 })
 export class DashboardModule {}
